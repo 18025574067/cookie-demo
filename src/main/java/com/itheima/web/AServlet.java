3f7744f9476d4ab0d9127e315f13036dc03081b3
aa <2419805602@qq.com>
2023-03-23 23:43:34 +0800

    home ubuntu16 ideaIU

--- src/main/java/com/itheima/web/AServlet.java
+++ src/main/java/com/itheima/web/AServlet.java
@@ -5,11 +5,15 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "AServlet", value = "/AServlet")
+@WebServlet("/aServlet")
 public class AServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // 发送cookie
+        // 1. 创建cookie对象
+        Cookie cookie = new Cookie("username", "hyb");
+        // 2. 发送cookie. response
+        response.addCookie(cookie);
     }
 
     @Override
