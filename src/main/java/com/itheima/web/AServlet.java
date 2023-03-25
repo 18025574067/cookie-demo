1cac0d67b47cb19870ff3d0ba649de3c2ba0b539
hyb <2419805602@qq.com>
2023-03-25 16:22:59 +0800

    home ubuntu16 ideaIU

--- src/main/java/com/itheima/web/AServlet.java
+++ src/main/java/com/itheima/web/AServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @WebServlet("/aServlet")
 public class AServlet extends HttpServlet {
@@ -11,7 +12,16 @@ public class AServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 发送cookie
         // 1. 创建cookie对象
-        Cookie cookie = new Cookie("username", "hyb");
+//        Cookie cookie = new Cookie("username", "hyb");
+        String value = "张三";
+        value = URLEncoder.encode(value);
+        System.out.println(value);
+
+        Cookie cookie = new Cookie("username", value);
+
+        //  设置存活时间（秒）: 1周7天
+        cookie.setMaxAge(60*60*24*7);
+
         // 2. 发送cookie. response
         response.addCookie(cookie);
     }
